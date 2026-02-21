a43faee41a8ec01d531fa6b4e976082762a0ea40
ACTIVERSE <mohammedrahmo051@gmail.com>
2026-02-21 05:09:46 +0200

    Create initial App component for the application

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>AI Casting Platform</h1>
+      <p>If you see this, the app is working.</p>
+    </div>
+  );
+}
